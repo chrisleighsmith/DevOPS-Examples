9fead849d74e3de596facbbbf6441e7ca1503497
Chris Smith <ciao@digrus.co.uk>
2019-02-03 13:00:21 +0000

    Tidy up

--- codeexamples/Python3/YouTube-Examples/Variables.py
+++ codeexamples/Python3/YouTube-Examples/Variables.py
@@ -1,5 +1,6 @@
+# Print Variables Example from Youtube Python3 video https://www.youtube.com/watch?v=rfscVS0vtbw&feature=youtu.be
 character_name = "David"
 character_age = "65"
 
 print ("Hi my name is " + character_name + ", I love coding now!")
-print ("Im think I'm doing quite well for my age of " + character_age + "do you think ?")
+print ("Im think I'm doing quite well for my age of " + character_age + " don't you think ?")
