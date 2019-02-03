8251e4853d4ce74984bbc0edb331bd86f7671e73
Chris Smith <ciao@digrus.co.uk>
2019-02-03 12:42:25 +0000

    New Print Variables example form Yourtube video Python3

---
+++ codeexamples/Python3/YouTube-Examples/Variables.py
@@ -0,0 +1,5 @@
+character_name = "David"
+character_age = "65"
+
+print ("Hi my name is " + character_name + ", I love coding now!")
+print ("Im think I'm doing quite well for my age of " + character_age + "do you think ?")
