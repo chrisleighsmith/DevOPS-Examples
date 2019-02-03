f5b3c82cb3129ee4c87ff5c80efb526c3b9fdced
Chris Smith <ciao@digrus.co.uk>
2019-02-03 13:57:11 +0000

    Changed Variable values in Second Sentence

--- codeexamples/Python3/YouTube-Examples/Variables.py
+++ codeexamples/Python3/YouTube-Examples/Variables.py
@@ -1,6 +1,13 @@
 # Print Variables Example from Youtube Python3 video https://www.youtube.com/watch?v=rfscVS0vtbw&feature=youtu.be
 character_name = "David"
-character_age = "65"
+character_age = "25"
 
-print ("Hi my name is " + character_name + ", I love coding now!")
-print ("Im think I'm doing quite well for my age of " + character_age + " don't you think ?")
+print ("Hi my name is " + character_name + ", I love coding, Im young")
+print ("I think I'm doing quite well but im only " + character_age + " years old.")
+
+#Chaniging the veriable values for the next sentances but not name of the variable
+character_name = "Steve"
+character_age = "75"
+
+print ("My Variable has been changed, so name is now " + character_name + ", I hate coding")
+print ("but Im am doing quite well for  " + character_age + " years old, don't you think ?")
